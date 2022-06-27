00cec9f9badd5ae40709db444f1679a6df95477e
VishnuV-21 <vishnuvs5171@gmail.com>
2022-06-27 16:05:51 +0530

    Second Commit

--- HelloGitHub/src/ClassOne.java
+++ HelloGitHub/src/ClassOne.java
@@ -1,6 +1,18 @@
 
 public class ClassOne {
+	
+	
+	public void haveyouMasteredGit(boolean status) {
+		if(status==true) {
+			System.out.println("Yes i have mastered");
+		}
+		else {
+			System.out.println("No its still in progress");
+		}
+	}
 public static void main(String[] args) {
-	System.out.println("I al,ost learned teh flow of github");
+	System.out.println("I almost learned teh flow of github");
+	ClassOne obj = new ClassOne();
+	obj.haveyouMasteredGit(false);
 }
 }
